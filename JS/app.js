ae48cc6441135f58ca0e153f3aaf41cc2f3ae359
BloodCMen <yakparedes14@gmail.com>
2026-01-06 11:50:19 -0500

    Deploy sitio principal G'isyl Mood

--- JS/app.js
+++ JS/app.js
@@ -22,6 +22,7 @@ const productos = [
         unidad: "piezas",
         imagen: "IMG/Card_Familia_estampado/Card_Pol_Familia_estamapado_01.webp",
         idSeccion: "Seccion-Familia"
+        
     },
     {
         nombre: "Pijamas Personalizadas",
